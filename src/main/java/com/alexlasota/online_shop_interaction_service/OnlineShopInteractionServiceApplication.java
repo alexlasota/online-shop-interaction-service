72ba0c84a4846f64650a9fc99034c2562a1a8aee
alexlasota <alexylasota@gmail.com>
2024-08-30 17:37:01 +0200

    test

---
+++ src/main/java/com/alexlasota/online_shop_interaction_service/OnlineShopInteractionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_interaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopInteractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopInteractionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlasota/online_shop_interaction_service/OnlineShopInteractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_interaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopInteractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
